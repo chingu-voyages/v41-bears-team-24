19496e5ac9e9b157d84a9a2a7dd4c1f2aa41fbe8
Joshua Hinman <joshuahinman@gmail.com>
2022-11-07 14:04:04 -0800

    added login state, menu items, menu categories to sesssion storage

--- frontEnd/order_up/src/App.tsx
+++ frontEnd/order_up/src/App.tsx
@@ -8,14 +8,39 @@ import Home from "./components/Home/Home";
 import Kitchen from "./components/Kitchen/Kitchen";
 import Admin from "./components/Admin/Admin";
 
+const EmptyUser = {createdAt: '', firstName: '', lastName: '', id: 0, password: '', role: '', username: ''};
 
+const getSessionValue = (key: string) => {
+  let value;
+  try {
+    value = JSON.parse( sessionStorage[key] );
+  }
+  catch {
+    value = null;
+  }
+  return value;
+}
 
 function App() {
-  const [menuCategories, setMenuCategories] = useState<any[]>([]);
-  const [menuItems, setMenuItems] = useState<any[]>([]);
+  const [menuCategories, setMenuCategories] = useState<any[]>(getSessionValue('menuCategories') ||[]);
+  const [menuItems, setMenuItems] = useState<any[]>(getSessionValue('menuItems') ||[]);
   const [activeOrders, setActiveOrders] = useState<cartOrder[]>([]);
-  const [loggedIn, setLoggedIn] = useState<User>({createdAt: '', firstName: '', lastName: '', id: 0,
-    password: '', role: '', username: ''});
+  const [loggedIn, setLoggedIn] = useState<User>(getSessionValue('loggedIn') || EmptyUser);
+
+  //save login data in session storage
+  useEffect(() => {
+    sessionStorage.loggedIn = JSON.stringify(loggedIn);
+  },[loggedIn]);
+
+  //save menu categories in session storage
+  useEffect(() => {
+    sessionStorage.menuCategories = JSON.stringify(menuCategories);
+  },[menuCategories]);
+
+  //save menu items in session storage
+  useEffect(() => {
+    sessionStorage.menuItems = JSON.stringify(menuItems);
+  },[menuItems]);
 
   const addNewOrder = (order: cartOrder) => {
     setActiveOrders(activeOrders.concat(order));
--- frontEnd/order_up/src/utils/api.ts
+++ frontEnd/order_up/src/utils/api.ts
@@ -4,6 +4,7 @@ import { CreateOrder } from "./backendTypes";
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
+//TODO: remove url string
 const API_BASE_URL = "https://v41-bears-team-24-production.up.railway.app"
    //process.env.REACT_APP_API_BASE_URL || "http://localhost:" + 5000;
 
