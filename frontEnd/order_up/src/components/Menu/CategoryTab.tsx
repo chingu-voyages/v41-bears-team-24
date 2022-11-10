c4b9ddbc6b4e3051a26ca7449c25f9eb46d11cfc
Philip Ziolkowski <philipz4848@gmail.com>
2022-11-09 16:30:18 -0800

    Additional menu page styling

--- frontEnd/order_up/src/components/Menu/CategoryTab.tsx
+++ frontEnd/order_up/src/components/Menu/CategoryTab.tsx
@@ -1,13 +1,15 @@
 interface CategoryTabProps {category: String,
-                        click: Function,
-                        value: number,
-                        activeTab: number};
-const liStyle = "m-2 text-white bg-blue-500 px-6 pb-1 border-solid border-2 border-indigo-700 rounded"
+    click: Function,
+    value: number,
+    activeTab: number};
+
+const liStyle = "m-2 text-white bg-blue-500 px-5 py-1 rounded-full transition duration-500 hover:scale-125 cursor-pointer"
+
 const CategoryTab = ({category, click, value, activeTab}: CategoryTabProps) => {
-    const bgColor = value === activeTab ? " bg-blue-900" : " bg-blue-500 hover:text-gray-300"
-    return (
-        <li onClick={ () => {click(value)} } className={liStyle + bgColor}>{category}</li>
-    )
+const bgColor = value === activeTab ? " bg-blue-900" : " bg-blue-500 "
+return (
+<li onClick={ () => {click(value)} } className={liStyle + bgColor}>{category}</li>
+)
 }
 
 export default CategoryTab;
